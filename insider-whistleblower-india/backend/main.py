f6017f2ac94a986f4e2b0a23baa8964bbb21a949
Codename-archie <archisman.saha2023@iem.edu.in>
2025-06-20 13:54:30 +0530

    Add backend initial FastAPI app and utils

--- insider-whistleblower-india/backend/main.py
+++ insider-whistleblower-india/backend/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "🚀 FastAPI is working!"}
+
+@app.get("/ping")
+async def ping():
+    return {"status": "ok"}
